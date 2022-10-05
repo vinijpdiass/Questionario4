19ff296ecc2045b0172aa14bea2d423cbc7bcd38
Sr.Mr.Calango <95763784+vinijpdiass@users.noreply.github.com>
2022-10-05 13:38:47 -0300

    Update Questão3.cs

--- Questão3.cs
+++ Questão3.cs
@@ -1,20 +1,24 @@
 using System;
 class questão3 {
   static void Main() {
-    int conjunto, numero, i,  menornumero = 1000000;
-      
-    Console.WriteLine("diga o tamanho do seu conjunto");
-    conjunto = int.Parse(Console.ReadLine());
-
-    for(i = 1; i <= conjunto; i++){
+    int menornumero = 100000;
+    int numero = 100000;  
+    
+    Console.WriteLine("diga os numeros presentes no seu conjunto\npara parar de adicionar ao conjunto digite 0");
+    
+    while (numero != 0){
         Console.WriteLine("escreva um numero inteiro");
         numero = int.Parse(Console.ReadLine());
         
-        if(numero < menornumero){
+        if(numero == 0){
+            break;
+        }
+        else if(numero < menornumero){
             menornumero = numero;
         }
-    }   
+    }
+    
     Console.WriteLine($"o menor numero do conjunto é {menornumero}"); 
-      
+    
   }
 }
