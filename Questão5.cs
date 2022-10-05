301993a56a99169fb7a6c82d7ab0f160a1f6409e
Sr.Mr.Calango <95763784+vinijpdiass@users.noreply.github.com>
2022-10-05 10:07:06 -0300

    Create Questão5.cs

---
+++ Questão5.cs
@@ -0,0 +1,27 @@
+using System;
+class questão3 {
+  static void Main() {
+    int impares = 0, pares = 0;
+    float numero = 0;  
+    
+    Console.WriteLine("diga os numeros presentes no seu conjunto\npara parar de adicionar ao conjunto digite -1");
+    
+    while (numero != -1){
+        Console.WriteLine("escreva um numero inteiro");
+        numero = float.Parse(Console.ReadLine());
+        
+        if(numero == -1){
+            break;
+        }
+        else if(numero % 2 == 0){
+            pares++;
+        }else{
+            impares++;
+        }
+    }
+    
+    Console.WriteLine($"a quantidade de numeros pares é {pares}");
+    Console.WriteLine($"a quantidade de numeros impares é {impares}");
+    
+  }
+}
